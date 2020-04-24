e96772c668de411daf46a27afc0a0c2c7f5d5b3c
Benny <snk2kimo@gmail.com>
2020-04-24 12:05:59 +0800

    計算長度

--- read.py
+++ read.py
@@ -8,8 +8,12 @@ with open('reviews.txt', 'r') as f: # 讀取reviews.txt 存入f
     for line in f:                  # 將f 用迴圈 每行一列出來
         data.append(line)           # 將line每行存入data清單
         count += 1                  # 等於 count = count + 1
-        if count % 1000 == 0:
+        if count % 10000 == 0:
             print(len(data))
-print(data[0])
-print('----------------------')
-print(data[1])
+print('讀取完了，總共有', len(data), '筆資料')
+
+numdata = 0
+for line2 in data:
+	numdata = numdata + len(line2)
+# 計算平均長度，取整數int
+print('留言平均長度為', int(numdata/len(data)), '個字')
