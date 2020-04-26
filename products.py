7119e059b00361e2251516d88a387f71206fc0ef
snk2kimo <64054888+snk2kimo@users.noreply.github.com>
2020-04-26 20:52:24 +0800

    Add find file

--- products.py
+++ products.py
@@ -1,15 +1,21 @@
+# 查詢檔案是否存在
+import os # 載入 os 模組
+
 # 讀取檔案
 products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue # 跳到下一個迴圈
-		name, price = line.strip().split(',') # 將line去調換行符號(strip).去掉逗號(split)後存入name, price兩個清單內
-		products.append([name, price])		
-print(products)
+if os.path.isfile('products.csv'):
+	print('Yeah!找到檔案了!')
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue # 跳到下一個迴圈
+			name, price = line.strip().split(',') # 將line去調換行符號(strip).去掉逗號(split)後存入name, price兩個清單內
+			products.append([name, price])		
+	print(products)
+else:
+	print('找不到檔案.....')
 
 # 記帳程式專案 - 讓使用者輸入
-products = []
 while True:
 	name = input('請輸入商品名稱： ')
 	if name == 'q':
